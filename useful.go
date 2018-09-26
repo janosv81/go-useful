37fde8cdd287e172f1cbcfa4c7ecfad761106288
janosv81 <virag.janos@gmail.com>
2018-09-26 13:30:48 +0100

    More methods

--- useful.go
+++ useful.go
@@ -50,3 +50,60 @@ func batchPostWithBearer(urls []string, bearerToken string) error {
 	}
 	return nil
 }
+
+func checkError(message string, err error) {
+	if err != nil {
+		log.Fatal(message, err)
+	}
+}
+
+//MapReduce reduce function for arrays
+func MapReduce(list []ExampleStruct, f func(ExampleStruct) string) []string {
+	vsm := make([]string, len(list))
+	for i, v := range list {
+		vsm[i] = f(v)
+	}
+	return removeDuplicates(vsm)
+}
+
+func removeDuplicates(elements []string) []string {
+	// Use map to record duplicates as we find them.
+	encountered := map[string]bool{}
+	result := []string{}
+
+	for v := range elements {
+		if encountered[elements[v]] == true {
+			// Do not add duplicate.
+		} else {
+			// Record this element as an encountered element.
+			encountered[elements[v]] = true
+			// Append to result slice.
+			result = append(result, elements[v])
+		}
+	}
+	// Return the new slice.
+	return result
+}
+
+//Filter filters arrays by function
+func Filter(vs []ExampleStruct, f func(ExampleStruct) bool) []ExampleStruct {
+	vsf := make([]ExampleStruct, 0)
+	for _, v := range vs {
+		if f(v) {
+			vsf = append(vs, v);
+		}
+	}
+	return vsf
+}
+
+func keyOfMaxValue(input map[string]int) string {
+	max := 0
+	maxKey := ""
+	for k, v := range input {
+		if v > max {
+			max = v
+			maxKey = k
+		}
+	}
+	return maxKey
+}
